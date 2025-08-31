aa6567fc2d99b8bc9214842f5e1f7f3f3e8eea11
rapha2e <julaisns1nsn@hotmail.com>
2025-08-31 19:26:51 -0300

    server.js

--- server.js
+++ server.js
@@ -22,9 +22,11 @@ mongoose.connect(process.env.MONGO_URI, {
   .catch(err => console.error("Erro ao conectar MongoDB:", err));
 
 // sobe servidor
-app.listen(3000, () => {
-  console.log("Servidor rodando na porta 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
+
 
 // ====== MODELO CLIENTE ======
 const clienteSchema = new mongoose.Schema({
