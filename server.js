b3b4173f5d7da6cd9e92542ba96ce8fb131c6226
rapha2e <julaisns1nsn@hotmail.com>
2025-08-31 17:17:47 -0300

    server.js
    ..

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
-git init
-git add .
-git commit -m "primeiro backend bandido"
-git branch -M main
-git remote add origin https://github.com/SEU-USUARIO/saas-bandido.git
-git push -u origin main
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.json());
+
+// rota de teste
+app.get("/", (req, res) => {
+  res.send("🔥 Backend do Lek tá on!");
+});
+
+// conecta no MongoDB Atlas
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB conectado 🔥"))
+  .catch(err => console.error("Erro ao conectar MongoDB:", err));
+
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
+});
